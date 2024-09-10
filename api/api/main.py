ab88a4f4a6f73488c1426e95b64b5d5d285746b2
Underscore76 <theunderscore76@gmail.com>
2024-09-09 21:44:30 -0500

    lets give it a whirl

--- api/api/main.py
+++ api/api/main.py
@@ -22,13 +22,3 @@ app.add_middleware(
 
 app.include_router(user.router)
 app.include_router(job.router)
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
